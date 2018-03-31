0ff056035174e7f79c072d750dc9c10a12ca77ba
Brad <bradley.frank@gmail.com>
2018-03-30 23:15:33 -0400

    Mac script that requires sudo permissions.

---
+++ plugins/macos.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Run this script as root
+
+# Set hostname
+scutil --set HostName Brads-Mac.local
+scutil --set LocalHostName Brads-Mac
+scutil --set ComputerName Brads-Mac
+
+dscacheutil -flushcache
+
+# Start locate daemon
+launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
+
+# Configure power settings
+pmset displaysleep 30
+systemsetup -setcomputersleep Never
+
+# Enable firewall
+/usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+
+# Enable FileVault
+fdesetup enable
